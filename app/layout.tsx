afb1530c8a858e71b0621023bacc62c78dd864be
unknown <abdoulrazackouedraogo1@gmail.com>
2025-01-23 16:27:21 +0000

    jour DB prosgres

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 
     <ClerkProvider>
       {/* theme : fantasy */}
-      <html lang="en" data-theme="light">
+      <html lang="en" data-theme="fantasy">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
